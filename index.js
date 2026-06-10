1d8c1d86c3925491a0c8397df8bec18724170279
Lucas <lucas@archistudios.com>
2026-06-10 23:46:54 +0200

    Add remaining routes

--- index.js
+++ index.js
@@ -28,6 +28,69 @@ app.get("/characters", async (req, res) => {
   }
 });
 
+app.get("/comics", async (req, res) => {
+  try {
+    const page = Number(req.query.page) || 1;
+    const title = req.query.title || "";
+
+    const limit = 100;
+    const skip = (page - 1) * limit;
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}&title=${title}`,
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.response?.data || error.message);
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+});
+
+app.get("/comics/:characterId", async (req, res) => {
+  try {
+    const characterId = req.params.characterId;
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${characterId}?apiKey=${process.env.MARVEL_API_KEY}`,
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.response?.data || error.message);
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+});
+
+app.get("/comic/:comicId", async (req, res) => {
+  try {
+    const comicId = req.params.comicId;
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comic/${comicId}?apiKey=${process.env.MARVEL_API_KEY}`,
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.response?.data || error.message);
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+});
+
+app.get("/character/:characterId", async (req, res) => {
+  try {
+    const characterId = req.params.characterId;
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/${characterId}?apiKey=${process.env.MARVEL_API_KEY}`,
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.response?.data || error.message);
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
